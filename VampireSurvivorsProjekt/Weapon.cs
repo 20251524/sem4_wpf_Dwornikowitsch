b09bbdb9c339a1a5dcb67dc37b9a1b2ff9c42f22
Max <20251524@htl-md.schule>
2026-04-28 15:19:17 +0200

    feat: Add weapong constructor

--- VampireSurvivorsProjekt/Weapon.cs
+++ VampireSurvivorsProjekt/Weapon.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace VampireSurvivorsProjekt
 {
     public class Weapon
     {
         double damage;
-        double attackSpeed;
         double range;
         double level;
+        double attacksPerSecond;
+        public double cooldownTimer;
+        public Weapon(double damage, double attacksPerSecond, double range)
+        {
+            this.damage = damage;
+            this.attacksPerSecond = attacksPerSecond;
+            this.range = range;
+            this.level = 1;
+            this.cooldownTimer = 0;
+        }
+
+
     }
 
-    public class Gun : Weapon
+    public class Fireball : Weapon
     {
+        public Fireball() : base(5, 1, 100) // Fireball konstruktor ruft die base klasse auf
+        {
+
+        }
 
     }
+
 }
