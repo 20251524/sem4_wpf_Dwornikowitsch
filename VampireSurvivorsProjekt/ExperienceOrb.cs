ee2b14bc47e96194e10110be2efc6e349dccca1d
Max <20251524@htl-md.schule>
2026-05-12 14:32:34 +0200

    feat: add ExperienceOrb class

---
+++ VampireSurvivorsProjekt/ExperienceOrb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace VampireSurvivorsProjekt
+{
+    public class ExperienceOrb
+    {
+        double OrbXPos;
+        double OrbYPos;
+        double xpValue;
+        Shape visual;
+    }
+}
