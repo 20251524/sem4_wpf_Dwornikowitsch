5ca1108a052f9b4b15d0194a57ae8e57318d67c2
Max <20251524@htl-md.schule>
2026-05-17 17:50:32 +0200

    feat: add DamageText class

---
+++ VampireSurvivorsProjekt/DamageText.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace VampireSurvivorsProjekt
+{
+    public class DamageText
+    {
+        public TextBlock visual;
+        public double xPos;
+        public double yPos;
+    }
+}
